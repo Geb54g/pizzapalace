e6c6da93cc4165505bf454181df09780b35e74df
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-03-23 17:53:55 +0300

    st:created media query

--- js/script.js
+++ js/script.js
@@ -5,65 +5,76 @@ let foodItems=[
         small:400,
         medium:600,
         large:900,
+        incart:0,
     },
     {
         name:'Hawaiian',
         small:600,
         medium:900,
         large:1400,
+        incart:0,
     },
     {
         name:'Pepperoni',
         small:500,
         medium:800,
         large:1100,
+        incart:0,
     },
     {
         name:'cheesy',
         small:550,
         medium:850,
         large:1200,
+        incart:0,
     },
     {
         name:'crispy',
         small:70,
         medium:80,
         large:100,
+        incart:0,
     },
     {
         name:'gluten free',
         small:80,
         medium:100,
         large:120,
+        incart:0,
     },{
         name:'stuffed',
         small:50,
         medium:70,
         large:100,
+        incart:0,
     },
     {
         name:'sausage',
         small:70,
         medium:95,
         large:120,
+        incart:0,
     },
     {
         name:'olives',
         small:50,
         medium:90,
         large:100,
+        incart:0,
     },
     {
         name:'pepperoni',
         small:100,
         medium:120,
         large:150,
+        incart:0,
     },
     {
         name:'mushroom',
         small:60,
         medium:80,
         large:90,
+        incart:0,  
     },
     {
         name:'delivery',
@@ -77,7 +88,11 @@ let foodItems=[
 }
 function cartItems(){
     let products = localStorage.getItem('cartItems');
-    products= parseInt(products)
+    products= parseInt(products);
+
+    if(products ){
+        localStorage.setItem('cartItems',1);
+    }
 }
 
 
